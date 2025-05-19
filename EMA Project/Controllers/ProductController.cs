da11e4bfa81f530cddcc927175ca15ccf0d4be94
AhmedAbdelhamid19 <ahmedaways200219@gmail.com>
2025-05-19 14:54:15 +0300

    add new API that Return Restaurants that offer product and distance to user

--- EMA Project/Controllers/ProductController.cs
+++ EMA Project/Controllers/ProductController.cs
@@ -76,9 +76,28 @@ namespace EMA_Project.Controllers
         }
 
 
+        [HttpGet("RestaurantsWithDistance/{id:int}/{latitude}/{longitude}")]
+        public IActionResult GetRestaurantsOfProductWithDistance(int id, double latitude, double longitude)
+        {
+            Product? product = DbRepository.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return BadRequest("Product not found");
+            }
+
+            var result = product.Restaurants
+                .Select(r => new
+                {
+                    Name = r.Name,
+                    Distance = RestaurantController.GetDistanceInKm(latitude, longitude, r.latitude, r.longtude)
+                }).ToList();
+
+            return Ok(result);
+        }
 
 
-        [HttpGet("RestaurantsByProductName/{name:regex(^[[a-zA-Z0-9\\s]]+$)}")]
+
+        [HttpGet("RestaurantsByProductName/{name:regex(^[[a-zA-Z0-9\\s]]+$)}/")]
         public IActionResult GetRestaurantsOfProductByName(string name)
         {
             Product? product = DbRepository.Products.FirstOrDefault(p => p.Name == name);
@@ -88,6 +107,27 @@ namespace EMA_Project.Controllers
             }
 
             return Ok(product.Restaurants);
+        }
+
+
+
+        [HttpGet("RestaurantsByProductNameWithDistance/{name:regex(^[[a-zA-Z0-9\\s]]+$)}/{latitude}/{longitude}")]
+        public IActionResult GetRestaurantsOfProductByNameWithDistance(string name, double latitude, double longitude)
+        {
+            Product? product = DbRepository.Products.FirstOrDefault(p => p.Name == name);
+            if (product == null)
+            {
+                return BadRequest("Product not found");
+            }
+
+            var result = product.Restaurants
+                .Select(r => new
+                {
+                    Name = r.Name,
+                    Distance = RestaurantController.GetDistanceInKm(latitude, longitude, r.latitude, r.longtude)
+                }).ToList();
+
+            return Ok(result);
         }
     }
 }
--- EMA Project/obj/Debug/net8.0/EMA Project.AssemblyInfo.cs
+++ EMA Project/obj/Debug/net8.0/EMA Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMA Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b1a488c392c61b23511d6429ef6618334b24b71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+447d082473a466ffe3bc8ecbf71fbcf0327e26d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMA Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMA Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
